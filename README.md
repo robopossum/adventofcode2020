ed9d112fc1ffdbe7bbc381bf862098d92d45c9cb
robopossum <robopossum@live.co.uk>
2020-12-13 02:30:06 +0000

    Made day 6 solutions faster by not reinstantiating regexp objects

--- README.md
+++ README.md
@@ -9,23 +9,23 @@ To run a specific day
 node run.js <day> <part>
 ```
 
-Runtimes from 2020-12-13 02:10:54
+Runtimes from 2020-12-13 02:29:13
 
 Day | Part | Time
 --- | --- | ---:
 1 | 1 | 1ms
-1 | 2 | 0ms
+1 | 2 | 1ms
 2 | 1 | 2ms
 2 | 2 | 1ms
 3 | 1 | 0ms
 3 | 2 | 0ms
-4 | 1 | 1ms
+4 | 1 | 2ms
 4 | 2 | 3ms
 5 | 1 | 2ms
 5 | 2 | 2ms
-6 | 1 | 5ms
-6 | 2 | 11ms
+6 | 1 | 3ms
+6 | 2 | 6ms
 7 | 1 | 2ms
-7 | 2 | 3ms
-8 | 1 | 1ms
-8 | 2 | 9ms
+7 | 2 | 2ms
+8 | 1 | 0ms
+8 | 2 | 10ms
--- day6/part2.js
+++ day6/part2.js
@@ -1,6 +1,6 @@
 module.exports = function (instructions) {
   var output = 0;
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const alphabet = [...Array(26)].map((_, index) => new RegExp(String.fromCharCode(index + 97)));
   var blob = [];
   instructions.forEach((instruction, index) => {
     if (instruction) {
@@ -12,7 +12,7 @@ module.exports = function (instructions) {
     alphabet.forEach(letter => {
       let count = 0;
       blob.forEach(answer => {
-        count += !!(new RegExp(letter)).exec(answer);
+        count += !!letter.exec(answer);
       });
       output += count === blob.length;
     });
